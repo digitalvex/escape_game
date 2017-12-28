707c742c5b9f59a883c19107609dc806c40ae851
digitalvex <mr.zachary.harvey@gmail.com>
2017-12-28 11:30:47 -0700

    initial commit

---
+++ Escape_Game/Source/Escape_Game/Escape_GameGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Escape_GameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPE_GAME_API AEscape_GameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Escape_Game/Source/Escape_GameEditor.Target.cs
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class Escape_GameEditorTarget : TargetRules
+{
+	public Escape_GameEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+
+		ExtraModuleNames.AddRange( new string[] { "Escape_Game" } );
+	}
+}
